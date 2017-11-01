87ca863ea49c164640a98dbd1e337d5647141125
Martin Mois <martin.mois@googlemail.com>
2017-11-01 16:00:32 +0100

    update to japicmp 0.11.0

--- src/main/java/japicmp/sonar/JApiCmpSensor.java
+++ src/main/java/japicmp/sonar/JApiCmpSensor.java
@@ -1,6 +1,6 @@
 package japicmp.sonar;
 
-import com.google.common.base.Optional;
+import japicmp.util.Optional;
 import japicmp.versioning.SemanticVersion;
 import japicmp.versioning.Version;
 import japicmp.versioning.VersionChange;
--- src/test/java/japicmp/sonar/JApiCmpSensorProcessClassesTest.java
+++ src/test/java/japicmp/sonar/JApiCmpSensorProcessClassesTest.java
@@ -1,6 +1,6 @@
 package japicmp.sonar;
 
-import com.google.common.base.Optional;
+import japicmp.util.Optional;
 import japicmp.versioning.SemanticVersion;
 import japicmp.xml.*;
 import org.junit.Before;
